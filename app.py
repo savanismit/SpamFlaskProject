6cc4bf469660f1789a8bcb401941287a309c3fe1
savanismit <savanismit11@gmail.com>
2020-05-12 18:18:45 +0530

    FrontEnd Done

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
-from flask import Flask,request,render_template,url_for
+from flask import Flask,request,render_template,url_for,flash
 import pickle
 import string
+from flask_bootstrap import Bootstrap
 from nltk.corpus import stopwords
 import os
 
 app = Flask(__name__)
+Bootstrap(app)
+app.config['SECRET_KEY'] = os.urandom(25)
 
-
-@app.route('/')
-def hello():
-    return render_template('index.html')
-
-def msg_processor(mess):
-    no_punc = [c for c in mess if c not in string.punctuation]
-    no_punc = ''.join(no_punc)
-    return [word for word in no_punc.split() if word.lower() not in stopwords.words('english')]
-
-@app.route('/predict',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict():
     if request.method == 'POST':
         mess = request.form['message']
@@ -27,8 +20,17 @@ def predict():
         pred = ''.join(pred)
         if pred == 'ham':
             my_prediction = "Normal Message"
+            mode = 'success'
         else:
             my_prediction = "It's a Spam Message"
-    return render_template('result.html',prediction = my_prediction)
+            mode = 'danger'
+        flash(my_prediction, mode)
+    return render_template('index.html')
+
+def msg_processor(mess):
+    no_punc = [c for c in mess if c not in string.punctuation]
+    no_punc = ''.join(no_punc)
+    return [word for word in no_punc.split() if word.lower() not in stopwords.words('english')]
+
 if __name__ == '__main__':
     app.run(debug=True)
